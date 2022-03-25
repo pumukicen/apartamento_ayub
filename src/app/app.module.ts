77191e3dec45a4294fe341176db006c6f8d54aea
Adrian Termenon <adrianriera@gmail.com>
2022-03-25 20:17:55 +0100

    error raro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
 import { ApartamentoComponent } from './pages/apartamento/apartamento.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     InicioComponent,
-    ApartamentoComponent
+    ApartamentoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
