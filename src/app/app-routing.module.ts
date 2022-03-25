3bd8405321672a4bbaa638c78db521fa8c24ee55
Adrian Termenon <adrianriera@gmail.com>
2022-03-25 20:15:31 +0100

    empezando con el menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ApartamentoComponent } from './pages/apartamento/apartamento.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'apartamento', component: ApartamentoComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/apartamento/apartamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apartamento',
+  templateUrl: './apartamento.component.html',
+  styleUrls: ['./apartamento.component.scss']
+})
+export class ApartamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
