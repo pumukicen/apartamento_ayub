aa13bb936296f88eadc39aa79fbe873eceb2c558
Adrian Termenon <adrianriera@gmail.com>
2022-03-29 20:42:54 +0200

    idiomas

---
+++ src/app/common/translations/translations.service.ts
@@ -0,0 +1,12 @@
+import { TRANSLATIONS, DEFAULT_LANG } from './translations';
+import { Injectable } from '@angular/core';
+import { TranslateLoader } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+export class TranslationsService implements TranslateLoader {
+  getTranslation(lang: string): Observable<{ [key: string]: string }> {
+    const translations = Object.entries(TRANSLATIONS).find((entry) => entry[0] === lang);
+    return of(translations ? translations[1] : DEFAULT_LANG);
+  }
+}
