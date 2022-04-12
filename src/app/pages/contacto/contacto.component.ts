dbbcba46630bdd6d511c3028b75390e439662970
Adrian Termenon <adrianriera@gmail.com>
2022-04-12 19:32:56 +0200

    mapa arreglado

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.scss']
+  styleUrls: ['./contacto.component.scss'],
 })
 export class ContactoComponent implements OnInit {
+  position = {
+    lat: 41.35281150296906,
+    lng: -1.643361734010334,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
